56f178abc3d40bd603ce8f273f9c400906d081f7
jh <bndgc@naver.com>
2021-12-15 21:25:48 +0900

    [Add] 수소만들기 - Summer/Winter Coding(~2018)

---
+++ programmers/python/level1/make_prime_number.py
@@ -0,0 +1,40 @@
+def get_prime_number(num):
+    if num == 1:
+        return False
+
+    elif num == 2:
+        return True
+
+    for i in range(3, num):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+def solution(nums):
+    answer = 0
+    record = set()
+
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                target = nums[i] + nums[j] + nums[k]
+                print(target)
+
+                if target not in record:
+                    if get_prime_number(target):
+                        record.add(target)
+    print(record)
+    return len(record)
+
+
+print(get_prime_number(9))
+print(solution([1, 2, 3, 4]))
+print('\n')
+print(solution([1, 2, 7, 6, 4]))
+print('\n')
+print(solution([1, 3, 5, 7, 9]))
+# 9 11 13 13 15 15 17 19 21
+
+print('\n')
