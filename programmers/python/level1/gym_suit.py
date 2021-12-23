32b256908beeeb04aa9e6cad4b8fab0dca3b7a5e
jh <bndgc@naver.com>
2021-12-23 22:23:05 +0900

    [Mod] 체육복 - 올바른 풀이

--- programmers/python/level1/gym_suit.py
+++ programmers/python/level1/gym_suit.py
@@ -1,34 +1,21 @@
 def solution(n, lost, reserve):
     answer = 0
+    lost.sort()
 
     for e in reversed(lost):
         if e in reserve:
-            lost.pop()  # 틀린 부분
+            lost.remove(e)
             reserve.remove(e)
 
-        elif e + 1 in reserve:
-            lost.pop()  # 틀린 부분
+    for e in reversed(lost):
+        if e+1 in reserve:
+            lost.remove(e)
             reserve.remove(e+1)
 
-        elif e - 1 in reserve:
-            lost.pop()  # 틀린 부분
+        elif e-1 in reserve:
+            lost.remove(e)
             reserve.remove(e-1)
 
     answer = n - len(lost)
 
     return answer
-
-
-# 틀린 이유
-'''
-lost = [1, 2, 3, 4, 5]
-reserve = [3, 5]
-
-for 에서 e가 3일때,
-의도한 동작은 3이 제거 되는 것이 지만
-4가 제거 된다.
-(속도 측면에서 remove 안쓰고 pop을 썼지만, 잘못된 선택)
-
-result: [1, 2, 3]
-
-'''
