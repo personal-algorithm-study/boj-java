b5c1fddbfe7ae8f5c85dddb61b3b77c0ed37fece
jh <bndgc@naver.com>
2021-12-27 19:32:07 +0900

    [Mod] 실패율 - Third try - 70.4/100 런타임 에러

--- programmers/python/level1/failure_rate.py
+++ programmers/python/level1/failure_rate.py
@@ -1,19 +1,15 @@
+# third_try
 def solution(N, stages):
-    answer = []
     users = len(stages)
     failure_rate_list = []
-    #failure_users = [stages.count(stage) for stage in range(1, N+1)]
-    #answer = [failure_users[i] / (users - failure_users[i-1]) for i in range(len(failure_users))]
 
     for stage in range(1, N+1):
-        failure_rate = stages.count(stage) / users
-        failure_rate_list.append(failure_rate)
-        users -= stages.count(stage)
+        failure_users = stages.count(stage)
+        failure_rate_list.append((stage, failure_users / users))
+        users -= failure_users
 
-    for _ in range(len(failure_rate_list)):
-        index = failure_rate_list.index(max(failure_rate_list))
-        failure_rate_list[index] = -1
-        answer.append(index + 1)
+    failure_rate_list.sort(key=lambda x: -x[1])
+    answer = [failure_rate[0] for failure_rate in failure_rate_list]
 
     return answer
 
