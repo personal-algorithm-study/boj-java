82863398b8b0489745d01cefe48a17c8ed7ccd74
jh <bndgc@naver.com>
2022-01-11 23:47:52 +0900

    [Mod] 카펫 완전제곱수 누락

--- programmers/python/kit/brute-force/carpet.py
+++ programmers/python/kit/brute-force/carpet.py
@@ -1,32 +1,40 @@
-#First
 def seek_divisor(number):
     if number == 1:
-        return [1]
-    
-    divisors = []
+        return {1}
+
+    divisors = set()
     divisor = 1
     i = 2
-    
+
+    if (number ** 0.5).is_integer():
+        divisors.add(int(number ** 0.5))
+
     while number != 1:
         if number % i == 0:
             divisor *= i
-            divisors.append(divisor)
-            divisors.append(number//i)
+            divisors.add(divisor)
+            divisors.add(number//i)
             number //= i
             i = 1
         i += 1
 
-    return divisors
+    return sorted(list(divisors))
+
 
 def solution(brown, yellow):
-    answer          = []
-    area            = brown + yellow
+    answer = []
+    area = brown + yellow
     yellow_divisors = seek_divisor(yellow)
-    
+    print(yellow_divisors)
+
     for divisor in yellow_divisors:
-        if (divisor+2)*((yellow//divisor)+2) == area:
-            answer = [(yellow//divisor)+2, divisor+2]
-            
-    answer.sort(reverse = True)
-    
+        divisor_pair = yellow//divisor
+
+        border = 2*(divisor+divisor_pair)+4
+
+        if border == brown:
+            answer = [divisor+2, divisor_pair+2]
+
+    answer.sort(reverse=True)
+
     return answer
