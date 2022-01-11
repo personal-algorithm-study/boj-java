b68b1451185cf039ed0502a7b3b2e96295ac1d3d
jh <bndgc@naver.com>
2022-01-12 00:42:50 +0900

    [Mod] 카펫 test case 7,8,9 실패 수정

--- programmers/python/kit/brute-force/carpet.py
+++ programmers/python/kit/brute-force/carpet.py
@@ -1,36 +1,13 @@
-def seek_divisor(number):
-    if number == 1:
-        return {1}
-
-    divisors = set()
-    divisor = 1
-    i = 2
-
-    if (number ** 0.5).is_integer():
-        divisors.add(int(number ** 0.5))
-
-    while number != 1:
-        if number % i == 0:
-            divisor *= i
-            divisors.add(divisor)
-            divisors.add(number//i)
-            number //= i
-            i = 1
-        i += 1
-
-    return sorted(list(divisors))
-
-
 def solution(brown, yellow):
     answer = []
-    area = brown + yellow
-    yellow_divisors = seek_divisor(yellow)
-    print(yellow_divisors)
 
-    for divisor in yellow_divisors:
+    for divisor in range(1, yellow+1):
+        if yellow % divisor != 0:
+            continue
+
         divisor_pair = yellow//divisor
 
-        border = 2*(divisor+divisor_pair)+4
+        border = 2*(divisor+divisor_pair) + 4
 
         if border == brown:
             answer = [divisor+2, divisor_pair+2]
