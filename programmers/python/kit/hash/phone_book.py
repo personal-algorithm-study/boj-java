fc59dcc0fd1f021143954a1cac3f5e6ef5b70f2c
jh <bndgc@naver.com>
2022-01-14 21:25:33 +0900

    [Mod] 전화번호 목록 second - try 11, 14 실패

--- programmers/python/kit/hash/phone_book.py
+++ programmers/python/kit/hash/phone_book.py
@@ -1,22 +1,21 @@
-# first - try
+# second try : 11, 14 실패
+import collections
+
+
 def solution(phone_book):
     phone_book.sort(key=lambda x: len(x))
-    hash_map = []
+    hash_map = collections.defaultdict(int)
 
-    for i in range(len(phone_book) - 1):
-        n = len(phone_book[i])
-        if i == 0 or len(phone_book[i]) != phone_book[i+1]:
-            hash_map = list(map(hash, [number[:n]
-                            for number in phone_book[i+1:]]))
+    n = len(phone_book[0])
 
-        else:
-            hash_map = hash_map[1:]
+    for number in phone_book:
+        for idx in range(n, len(number)+1):
+            if hash_map[number[:idx]] == 0:
+                hash_map[number[:idx]] += 1
+                # print(hash_map)
+            else:
+                return False
 
-        if hash(phone_book[i]) in hash_map:
-            return False
-
-        elif phone_book[i]:
-            pass
     return True
 
 #print(solution(["119", "97674223", "1195524421"]))
