fc8471c4da3c6024273e58a87e8aab0f24fd3ffe
김주형 <bufflect@gimjuhyeong-ui-MacBookAir.local>
2022-01-21 20:25:12 +0900

    [Add] 더 맵게

---
+++ programmers/python/kit/heap/more_spicy.py
@@ -0,0 +1,31 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    
+    heapq.heapify(scoville)
+    print(scoville)
+    
+    while scoville:
+        answer += 1
+        
+        first  = heapq.heappop(scoville) 
+        second = heapq.heappop(scoville)
+        new    = first + second*2
+        
+        heapq.heappush(scoville, new)
+        
+        if scoville[0] >= K:
+            return answer
+    
+    answer = - 1
+    
+    return answer
+
+s = [1, 2, 3, 9, 10, 12]
+x = [1]
+y = [1, 2]
+
+print(solution(s, 7))
+print(solution(x, 1))
+print(solution(y, 2))
