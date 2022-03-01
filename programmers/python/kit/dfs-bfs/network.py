ba373f01e657e116e103e62579f73342cd81354a
JH <bufflect@jh-ui-MacBookAir.local>
2022-03-02 01:34:07 +0900

    [Mod] 네트워크 1과 맞닿지 않는 노드 탐색하도록 수정 더 많은 오답

--- programmers/python/kit/dfs-bfs/network.py
+++ programmers/python/kit/dfs-bfs/network.py
@@ -6,11 +6,16 @@ def solution(n, computers):
     def dfs(v):
         nonlocal network
         for i, v in enumerate(computers[v]):
-            if not visited[i] and v == 1:
+            if not visited[i]:
                 visited[i] = True
-                network -= 1
+                if v == 1:
+                    network -= 1 
                 dfs(i)
     
     dfs(0)
     
     return network
+    
+# print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+# print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
+print(solution(4, [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]]))
