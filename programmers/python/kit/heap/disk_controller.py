58b04c3fe6a471a889f9f967aa8eef98825f277c
JH <bufflect@jh-ui-MacBookAir.local>
2022-03-16 23:25:25 +0900

    [Mod] 디스크 컨트롤러 다른 사람 풀이

--- programmers/python/kit/heap/disk_controller.py
+++ programmers/python/kit/heap/disk_controller.py
@@ -1,35 +1,22 @@
-def solution(jobs):
-    # n = len(jobs)
-    total = 0
-    time = 0
-    jobs.sort(key=lambda x: x[1])
-    i = 0
+import heapq 
 
+def solution(jobs): 
+    answer, now, i = 0, 0, 0 
+    start = -1 
+    heap = [] 
+    
     while i < len(jobs):
-        if jobs[i][0] <= time and jobs[i][1] != -1:
-            total += time - jobs[i][0] + jobs[i][1]
-            time += jobs[i][1]
-            i += 1
-
-        elif jobs[i][1] == -1:
-            i += 1
-
-        else:
-            j = i + 1
-            ifb = 0
-            # while j < len(jobs):
-            for j in range(i + 1, len(jobs)):
-                if jobs[j][0] <= time:
-                    total += time - jobs[j][0] + jobs[j][1]
-                    time += jobs[j][1]
-                    jobs[j][1] = -1
-                    ifb = 1
-                    break
-            
-            # 추가
-            if ifb == 0:
-                time += 1
-            
-            continue
-
-    return total // len(jobs)
+        for j in jobs: 
+            if start < j[0] <= now: 
+                heapq.heappush(heap, [j[1], j[0]]) 
+        
+        if len(heap) > 0: 
+            cur = heapq.heappop(heap) 
+            start = now 
+            now += cur[0] 
+            answer += now - cur[1] 
+            i +=1 
+        else: 
+            now += 1 
+                
+    return answer // len(jobs)
