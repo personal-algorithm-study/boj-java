9c71767deeee691cdb53687171809a01f59d6413
JH <bufflect@jh-ui-MacBookAir.local>
2022-03-18 23:39:04 +0900

    [Mod] 단어 변환 third-try bfs

--- programmers/python/kit/dfs-bfs/word_transform.py
+++ programmers/python/kit/dfs-bfs/word_transform.py
@@ -5,6 +5,14 @@ def solution(begin, target, words):
     if target not in words:
         return 0
 
+    def compare(first, second):
+        cnt = 0
+        for i, j in zip(first, second):
+            if i != j:
+                cnt += 1
+
+        return cnt
+
     queue = collections.deque()
     cnt = 0
     h = {}
@@ -15,7 +23,8 @@ def solution(begin, target, words):
         now, cnt = queue.popleft()
         cnt += 1
         for word in words:
-            if len(set(word) - set(now)) == 1 or len(set(now) - set(word)) == 1: # 문제
+            # if len(set(word) - set(now)) == 1 or len(set(now) - set(word)) == 1:
+            if compare(now, word) == 1:
                 queue.append((word, cnt))
                 if word not in h:
                     h[word] = cnt
@@ -25,3 +34,11 @@ def solution(begin, target, words):
             words.remove(now)
 
     return h[target]
+
+
+# print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+# print(solution("hit", "cog", ["cog", "log", "lot", "dog", "dot", "hot"]))
+# print(solution("aoa", "aof", ["aob", "aoc", "aod", "aof", "aoe"]))
+# print(solution("aaa", "aab", ["aab"]))
+# print(solution("hit", "hhh", ["hhh", "hht"]))
+print(solution('aaa', 'abc', ['aaz', 'aab', 'abb', 'abc']))
