523940c35cbf0e089d4dfe04b451202649c68e2f
JH <bufflect@jh-ui-MacBookAir.local>
2022-03-23 23:22:45 +0900

    [Mod] 가장 먼 노드 다익스트라 양방향 수정

--- programmers/python/kit/graph/most_far_node.py
+++ programmers/python/kit/graph/most_far_node.py
@@ -2,30 +2,30 @@ import heapq
 
 
 def solution(n, edge):
-    answer = 0
     INF = int(1e9)
+    answer = 0
+    graph = [[] for _ in range(n + 1)]
+    visited = [True]
     distance = [INF] * (n + 1)
     distance[1] = 0
-    graph = [[] for i in range(n + 1)]
 
-    for v, u in edge:
-        if v < u:
-            graph[v].append((u, 1))
-        else:
-            graph[u].append((v, 1))
+    for u, v in edge:
+        # if u < v:
+        graph[u].append((1, v))
+        # else:
+        graph[v].append((1, u))
 
     heap = []
-
     heapq.heappush(heap, (0, 1))
 
     while heap:
-        way_point_cost, v = heapq.heappop(heap)
+        initial_cost, v = heapq.heappop(heap)
 
-        if distance[v] < way_point_cost:
-            continue
+        # if distance[way_point] < initial_cost:
+        # print(1)
 
-        for u, cost in graph[v]:
-            total_cost = cost + way_point_cost
+        for cost, u in graph[v]:
+            total_cost = initial_cost + cost
             if total_cost < distance[u]:
                 distance[u] = total_cost
                 heapq.heappush(heap, (total_cost, u))
@@ -34,4 +34,9 @@ def solution(n, edge):
         if distance[i] == INF:
             distance[i] = -1
 
-    return distance.count(max(distance))
+    answer = distance.count(max(distance))
+
+    return answer
+
+
+print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
