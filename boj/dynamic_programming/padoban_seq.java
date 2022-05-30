54a624748da96bd4ebbc3fe3ebce6f607f73f79d
jh <bndgc@naver.com>
2022-05-30 23:48:45 +0900

    [Add] 파도반 수열 - 오버플로우 수정

--- boj/dynamic_programming/padoban_seq.java
+++ boj/dynamic_programming/padoban_seq.java
@@ -4,20 +4,11 @@ import java.util.Arrays;
 import java.io.IOException;
 
 public class Main {
-    static int[] dp;
+    static long[] dp = new long[101];
     static StringBuilder sb = new StringBuilder();
 
     public static void solution(int n) {
-        dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 1;
-        dp[3] = 1;
-
-        dp[4] = 2;
-        dp[5] = 2;
-        dp[6] = 3;
-
-        if (n < 6) {
+        if (dp[n] != 0) {
             sb.append(dp[n]).append('\n');
             return;
         }
@@ -37,6 +28,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[6] = 3;
+
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
             solution(n);
