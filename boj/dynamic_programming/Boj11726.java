2386b669a7c45ac1f3f185c60f20821323e159b6
jh <bndgc@naver.com>
2022-05-31 23:47:40 +0900

    [Add] boj11726 2xn 타일링

---
+++ boj/dynamic_programming/Boj11726.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void solution(int n) {
+        if (n <= 2) {
+            sb.append(n).append("\n");
+            return;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        sb.append(dp[n]).append("\n");
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        solution(n);
+
+        System.out.println(sb.toString());
+
+        return;
+    }
+}
