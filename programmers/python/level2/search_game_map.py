2a9c1cab6911a863a84aba2b26d1a48dc6b4d461
bufflect <bndgc@naver.com>
2022-06-30 23:28:56 +0900

    [Mod] 게임맵 탐색 - bfs 성공

--- programmers/python/level2/search_game_map.py
+++ programmers/python/level2/search_game_map.py
@@ -1,3 +1,4 @@
+import collections
 import sys
 
 dx = [0, 1, 0, -1]
@@ -7,26 +8,23 @@ dy = [1, 0, -1, 0]
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-    result = 1
-    answer = sys.maxsize
 
-    def dfs(x, y):
-        nonlocal result, answer
-        if x == n - 1 and y == m - 1:
-            answer = min(answer, result)
-            return
+    answer = sys.maxsize
+    visited = [[0] * m for _ in range(n)]
+    queue = collections.deque()
+    queue.append((0, 0))
+
+    while queue:
+        x, y = queue.popleft();
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
-                result += 1
-                maps[x][y] = 0
-                dfs(nx, ny)
-                result -= 1
-                maps[x][y] = 1
+            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 and visited[nx][ny] == 0:
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx, ny))
 
-    dfs(0, 0)
+    return -1 if visited[n - 1][m - 1] == 1 else visited[n - 1][m - 1] + 1
 
-    return -1 if answer == sys.maxsize else answer
+print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
