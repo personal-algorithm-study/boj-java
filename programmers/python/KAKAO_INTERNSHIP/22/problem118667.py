5ed0a02bfe1d2faa8d1ebaaa75070a9f91a74be7
bufflect <bndgc@naver.com>
2022-09-15 23:57:44 +0900

    [mod] 118667 두 개의 큐 합 같게 만들기 - 오타 수정

--- programmers/python/KAKAO_INTERNSHIP/22/problem118667.py
+++ programmers/python/KAKAO_INTERNSHIP/22/problem118667.py
@@ -21,4 +21,4 @@ def solution(queue1, queue2):
 
 print(solution([3, 2, 7, 2], [4, 6, 5, 1]) == 2)
 print(solution([1, 2, 1, 2], [1, 10, 1, 2]) == 7)
-print(solution([1, 1], [1, 5]) == -1ㅑ)
+print(solution([1, 1], [1, 5]) == -1)
