910e72c2414f3fc5ba382c0b4869589d371d7c6f
bufflect <bndgc@naver.com>
2022-10-10 23:15:31 +0900

    [Add]43162: 네트워크 - 성공

---
+++ programmers/python/level3/problem43162.py
@@ -0,0 +1,21 @@
+def solution(n, computers):
+    visited = [False] * n
+    network = n
+    visited[0] = True
+
+    def dfs(v):
+        nonlocal network
+        for i, v in enumerate(computers[v]):
+            if not visited[i]:
+                visited[i] = True
+                if v == 1:
+                    network -= 1
+                dfs(i)
+        return
+
+    dfs(0)
+    return network
+
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]) == 2)
+print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]) == 1)
