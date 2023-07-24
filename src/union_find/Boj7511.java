bacf2193d54b5b13dc32d3103a41bf0c0e364597
bufflect <bndgc@naver.com>
2023-07-24 21:39:44 +0900

    [Add] Boj7511 소셜 - 실패:메모리 초과  - static parent 때문인 것으로 예상 했으나, 그것으로 성공 받은 사람이 있음;;

---
+++ src/union_find/Boj7511.java
@@ -0,0 +1,65 @@
+package union_find;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Boj7511 {
+	static int find(int[] parent, int a) {
+		if (parent[a] != a) {
+			parent[a] = find(parent, parent[a]);
+		}
+		return parent[a];
+	}
+
+	static void union(int[] parent, int a, int b) {
+		int pa = find(parent, a);
+		int pb = find(parent, b);
+
+		if (pa < pb) {
+			parent[pa] = pb;
+		} else {
+			parent[pb] = pa;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			System.out.println("Scenario " + (i + 1) + ":");
+
+			int n = Integer.parseInt(br.readLine());
+			int k = Integer.parseInt(br.readLine());
+			int[] parent = new int[n];
+
+			for (int j = 0; j < n; j++) {
+				parent[j] = j;
+			}
+
+			int[] inputs;
+			for (int j = 0; j < k; j++) {
+				inputs = Arrays.stream(br.readLine().split(" "))
+						.mapToInt(Integer::parseInt)
+						.toArray();
+				union(parent, inputs[0], inputs[1]);
+			}
+
+			int m = Integer.parseInt(br.readLine());
+			for (int j = 0; j < m; j++) {
+				inputs = Arrays.stream(br.readLine().split(" "))
+						.mapToInt(Integer::parseInt)
+						.toArray();
+				if (find(parent, inputs[0]) == find(parent, inputs[1])) {
+					System.out.println("1");
+				} else {
+					System.out.println("0");
+				}
+			}
+		}
+
+		br.close();
+	}
+}
