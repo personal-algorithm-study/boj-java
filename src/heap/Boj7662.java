7fef255fdb8ab75a97a758186ed6b97cc25cb31f
bufflect <bndgc@naver.com>
2023-09-05 17:53:43 +0900

    [Add] (골드 ?) boj7662 이중 우선순위 큐 - 실패

---
+++ src/heap/Boj7662.java
@@ -0,0 +1,66 @@
+package heap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+// (?) boj 7662 이중 우선 순위 큐
+public class Boj7662 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		/*
+		* 이중 우선 순위 큐
+		* I 삽입
+		* D 1 최댓값 삭제
+		* D -1 최솟값 삭제
+		* */
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+
+		for (int t = 0; t < T; t++) {
+			int K = Integer.parseInt(br.readLine());
+
+			for (int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+
+				String order = st.nextToken();
+				Integer number = Integer.parseInt(st.nextToken());
+
+				if ("I".equals(order)) {
+					map.put(number, map.getOrDefault(number, 0) + 1);
+				} else if ("D".equals(order)) {
+					int now;
+
+					if (map.isEmpty()) continue;
+					if (number == 1)
+						now = map.lastKey();
+					else
+						now = map.firstKey();
+
+					int cnt = map.get(now);
+					if (cnt <= 1) {
+						map.remove(now);
+					} else {
+						map.put(now, cnt - 1);
+					}
+				}
+				System.out.println(map);
+			}
+
+			// System.out.println();
+			if(map.isEmpty()) {
+				sb.append("EMPTY");
+			} else {
+				sb.append(map.lastKey()).append(' ').append(map.firstKey());
+			}
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
