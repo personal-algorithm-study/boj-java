1d9df871a7bfcb50e815cf6467aedb155fdc1a07
bufflect <bndgc@naver.com>
2024-01-24 23:29:13 +0900

    [Add] (골드3) boj7579 앱 - 성공

--- src/dynamic_programming/Boj7579.java
+++ src/dynamic_programming/Boj7579.java
@@ -5,44 +5,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// (골드3) boj7579 앱 - 실패
+// (골드3) boj7579 앱 - 성공
 // 냅색 문제, 다이나믹 프로그래밍
 public class Boj7579 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		// 입력 받기
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int TOTAL = 0;
+		int N = Integer.parseInt(st.nextToken()); // 앱의 수
+		int M = Integer.parseInt(st.nextToken()); // 확보해야 하는 메모리
 
-		int[] memories = new int[N + 1];
-		int[] costs = new int[N + 1];
+		int[] memories = new int[N];
+		int[] costs = new int[N];
+		int answer = 0;
+
+		int[] dp = new int[10001];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			memories[i + 1] = Integer.parseInt(st.nextToken());
+			memories[i] = Integer.parseInt(st.nextToken());
 		}
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			costs[i + 1] = Integer.parseInt(st.nextToken());
-			TOTAL += costs[i + 1];
+			costs[i] = Integer.parseInt(st.nextToken());
 		}
 
-		// dp 시작
-		int answer = Integer.MAX_VALUE;
-		int[] dp = new int[TOTAL + 1];
+		for (int i = 0; i < N; i++) {
+			for (int j = 10000; j >= 0; j--) {
+				if (j - costs[i] < 0)
+					continue;
+				int possibleMemory = dp[j - costs[i]] + memories[i];
+				if (dp[j] < possibleMemory) {
+					dp[j] = possibleMemory;
+				}
+			}
+		}
 
-		for (int n = 1; n < N + 1; n++) {
-			int memory = memories[n], cost = costs[n];
-			for (int c = TOTAL; c > 0; c--) {
-				if (c - cost < 0)
-					break;
-				dp[c] = Math.max(dp[c], dp[c - cost] + memory);
-				if (dp[c] >= M)
-					answer = Math.min(answer, c);
+		for (int i = 0; i < 10001; i++) {
+			if (dp[i] >= M) {
+				answer = i;
+				break;
 			}
 		}
 		System.out.println(answer);
